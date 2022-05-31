c6fa60933a1dc074cdc9c6a292ddeeb59855d1bb
Alexey Pisanko <apis@users.noreply.github.com>
2022-05-31 15:46:36 -0400

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace NetCoreSpa;
+
+/// <summary>
+///     Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    private void OnStartup(object sender, StartupEventArgs eventArgs)
+    {
+        Task.Run(() =>
+        {
+            var builder = WebApplication.CreateBuilder();
+            builder.Services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/build"; });
+            builder.WebHost.UseUrls("http://localhost:16000", "https://localhost:16001");
+
+            var app = builder.Build();
+
+            app.UseSpaStaticFiles();
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ClientApp";
+
+                if (app.Environment.IsDevelopment()) 
+                    spa.UseReactDevelopmentServer(npmScript: "start");
+            });
+
+            // app.MapGet("/hello", () => "Hello World!");
+
+            // app.Run("http://localhost:16000;https://localhost:16001");
+            app.Run();
+        });
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Windows;
+using Microsoft.Web.WebView2.Core;
+
+namespace NetCoreSpa;
+
+/// <summary>
+///     Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        InitializeAsync();
+    }
+
+    private async void InitializeAsync()
+    {
+        var coreWebView2Environment = await CoreWebView2Environment.CreateAsync(userDataFolder: Path.GetTempPath());
+        await WebView.EnsureCoreWebView2Async(coreWebView2Environment);
+        WebView.CoreWebView2.Navigate("http://localhost:16000/");
+    }
+}
