ea1fe9837b06d6fd60691e74e6cb89233d03f3f3
Alexey Pisanko <apis@users.noreply.github.com>
2022-06-22 11:51:02 -0400

    Code clean up

--- WebApplication/web-frontend/src/App.vue
+++ WebApplication/web-frontend/src/App.vue
@@ -1,15 +1,54 @@
 <template>
-  <a>NavbarBrand</a>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/counter">Counter</router-link> |
-    <router-link to="/fetch-data">Fetch data</router-link>
-  </nav>
-  <router-view/>
+  <header>
+    <nav class="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3 navbar navbar-light">
+      <div class="container">
+        <a class="navbar-brand" href="/">DesktopSpa</a>
+        <button aria-label="Toggle navigation" type="button" class="mr-2 navbar-toggler">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="d-sm-inline-flex flex-sm-row-reverse collapse navbar-collapse">
+          <ul class="navbar-nav flex-grow">
+            <li class="nav-item">
+              <router-link to="/" class="text-dark nav-link">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/counter" class="text-dark nav-link">Counter</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/fetch-data" class="text-dark nav-link">Fetch data</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+  <div class="container">
+    <router-view />
+  </div>
 </template>
 
 <style>
+a.navbar-brand {
+  white-space: normal;
+  text-align: center;
+  word-break: break-all;
+}
 
+html {
+  font-size: 14px;
+}
+
+@media (min-width: 768px) {
+  html {
+    font-size: 16px;
+  }
+}
+
+.box-shadow {
+  box-shadow: 0 .25rem .75rem rgba(0, 0, 0, .05);
+}
+
+/* Provide sufficient contrast against white background */
 a {
   color: #0366d6;
 }
@@ -23,29 +62,4 @@ code {
   background-color: #1b6ec2;
   border-color: #1861ac;
 }
-
-@import "@/assets/custom.css";
-@import "@/assets/NavMenu.css";
-
-  /*#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-*/
 </style>
